7ae0af3c3548c6b10f4949f43087081c9bb2f783
HP <paul_system@hotmail.com>
2017-01-22 00:25:57 -0500

    una actualizacion

---
+++ Code/proinstituto/app/Http/Controllers/inicioController.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: HP
+ * Date: 21/01/2017
+ * Time: 11:34 PM
+ */
+namespace App\Http\Controllers;
+
+class inicioController extends Controller{
+    public function inicio(){
+        return "Hola mundo de laravel";
+    }
+}
